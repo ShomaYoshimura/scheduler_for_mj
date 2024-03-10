342547baf25f124f2d0ff1e5fedd171e5ab4e3ec
ShomaYoshimura <shomayoshimura1@gmail.com>
2024-03-11 01:26:16 +0900

    htmlタグとbodyタグの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import SideBar from "@/components/SideBar";
-import Head from "next/head";
-
-
-const inter = Inter({ subsets: ["latin"] });
+import RootLink from "@/components/Router";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "scheduler_for_mj",
@@ -16,19 +12,33 @@ type Props = {
 
 }
 
-export default function RootLayout({children}: Props) {
+export default function RootLayout({children}:Props) {
   return (
 <>
-  <Head>
-    <title>charts-sample</title>
-  </Head>
-  <div className="flex h-screen">
-    <SideBar />
-    {/* メインコンテンツ */}
-    <main className="flex-1 bg-gray-200">
-      {children}
-    </main>
-  </div>
+<html lang="ja">
+  <body>
+    <div className="flex">
+      <div className="flex justify-center items-center">
+        <h3>menu</h3>
+        <RootLink
+        root = "/"
+        displayWord="TOPPAGE"
+        />
+        <RootLink
+        root = "/schedule"
+        displayWord="schedule"
+        />
+        <RootLink
+        root = "/result"
+        displayWord="result"
+        />
+      </div>
+      <div className="bg-gray-200">
+        {children}
+      </div>
+    </div>
+  </body>
+</html>
 </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Link from "next/link";
-import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     TOPPAGE
-    </main>
+    <div className="text-center">
+      TOPPAGE
+    </div>
   );
 }
